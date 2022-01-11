155c7d8673d76b424608431c953a376f321295ec
Ozkanil <ozkanilhan@gmail.com>
2022-01-11 13:21:50 +0300

    Changes commit

--- src/testing/Github_Test2.java
+++ src/testing/Github_Test2.java
@@ -3,5 +3,6 @@ package testing;
 public class Github_Test2 {
     public static void main(String[] args) {
         System.out.println("TEst 2");
+        System.out.println("addition to the project");
     }
 }
